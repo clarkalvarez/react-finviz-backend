158720a0f81fe23848722fb9905d8f34e3b90299
clarkalvarez <43803904+clarkalvarez@users.noreply.github.com>
2022-07-08 11:29:26 +0800

    Add files via upload

---
+++ news.js
@@ -0,0 +1,56 @@
+const { initializeBrowser } = require("./initializeBrowser");
+
+module.exports = (function () {
+  const service = {};
+  service.news = async function (stockCode) {
+    const [browser, page] = await initializeBrowser(stockCode);
+
+    const firstRow = await page.evaluate(() => {
+      return Array.from(
+        document.querySelectorAll("#news-table tbody tr td:first-child")
+      ).map((x) => x.textContent.trim());
+    });
+
+    const secondrow = await page.evaluate(() => {
+      return Array.from(
+        document.querySelectorAll("#news-table tbody tr td:nth-child(2)")
+      ).map((x) => x.textContent.trim());
+    });
+
+    const rowHref = await page.evaluate(() => {
+      return Array.from(
+        document.querySelectorAll(
+          "#news-table tbody tr td:nth-child(2) .news-link-container .news-link-left a[href]"
+        )
+      ).map((a) => a.getAttribute("href"));
+    });
+
+    await browser.close();
+    let news = {
+      stockCode: { value: stockCode },
+    };
+    let currentDate;
+    firstRow.map((e, i) => {
+      const newsSplit = e.split(" ");
+      let date;
+      let time;
+      if (newsSplit.length === 2) {
+        date = newsSplit[0];
+        time = newsSplit[1];
+        currentDate = date;
+      } else {
+        date = currentDate;
+        time = newsSplit[0];
+      }
+      const timeObj = {
+        [time]: {
+          title: secondrow[i],
+          link: rowHref[i],
+        },
+      };
+      news[date] = Object.assign(timeObj, news[date]);
+    });
+    return await news;
+  };
+  return service;
+})();

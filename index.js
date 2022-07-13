c5c92d60811c698a197c7010f0a650c1f8848428
clarkalvarez <clarknikka012@gmail.com>
2022-07-13 12:33:45 +0800

    added changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
 const db = require("./api/models");
-const PORT = process.env.PORT || 8080;
+var port = process.env.PORT || 4000;
 
 app.use(
   cors({
